b28964711baf6ff524ea54f51a3e1327394846ad
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-05-21 11:30:36 +0300

    1.3. JavaDoc. Code convention[#143203]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,11 +1,25 @@
 package ru.job4j.calculate;
 
+/**
+* Print specified word three times.
+*
+* @author Andreev Aleksandr (andreev.aleksandr.spb@gmail.com)
+*/
 public class Calculate {
+	
+	/**
+	* Main method.
+	* @param args - arguments to run the program.
+	*/
     public static void main(String[] args) {
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("hey"));
     }
 	
+	/**
+	* Сreate a new string based on input argument.
+	* @return string consisting of repeated three times the value of the argument.  
+	*/
 	public String echo(String value) {
 			return String.format("%s %s %s", value, value, value);
 	}	
