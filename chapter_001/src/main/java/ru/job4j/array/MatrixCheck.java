cce43bef0f0abc6d7930d9966ca9e5133185816e
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-05-23 13:22:07 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#143181]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Matrix diagonal monotony check.
+ *
+ * @author Andreev Aleksandr (andreev.aleksandr.spb@gmail.com)
+ * @version 1
+ * @since 23.05.2019
+ */
+public class MatrixCheck {
+
+    /**
+     * Main calculations.
+     *
+     * @param data input matrix
+     * @return if diagonal is monotone.
+     */
+    public boolean mono(boolean[][] data) {
+        int length = data.length;
+        for (int i = 1; i < length; i++) {
+            if ((data[i - 1][i - 1] != data[i][i]) || (data[i - 1][length - i] != data[i][length - i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
