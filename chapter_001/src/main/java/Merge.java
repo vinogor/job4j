67c8e89ae1bb10f84cd45dab4659be7e242dc0c2
vinogor <andreev.aleksandr.spb@gmail.com>
2019-05-25 10:19:02 +0300

    examination task to the chapter 001

---
+++ chapter_001/src/main/java/Merge.java
@@ -0,0 +1,34 @@
+
+public class Merge {
+	public int[] merge(int[] left, int[] right) {
+
+		int lenLeft = left.length;
+		int lenRight = right.length;
+		int[] rsl = new int[lenLeft + lenRight];
+		int indexLeft = 0;
+		int indexRight = 0;
+
+		for (int i = 0; i < lenLeft + lenRight; i++) {
+			if ((indexLeft == lenLeft) && (indexRight == lenRight)) {
+				return rsl;
+			} else {
+				if (indexLeft == lenLeft) {
+					rsl[i] = right[indexRight];
+					indexRight++;
+				} else if (indexRight == lenRight) {
+					rsl[i] = left[indexLeft];
+					indexLeft++;
+				} else {
+					if (left[indexLeft] < right[indexRight]) {
+						rsl[i] = left[indexLeft];
+						indexLeft++;
+					} else {
+						rsl[i] = right[indexRight];
+						indexRight++;
+					}
+				}
+			}
+		}
+		return rsl;
+	}
+}
