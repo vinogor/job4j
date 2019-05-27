9b488c632f9336d9fa3913972a7a2eeb98a50b88
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-05-27 10:20:06 +0300

    1. Рефакторинг - Расстояние между точками.[#143269]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -3,8 +3,40 @@ package ru.job4j.condition;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
+/**
+ * Класс в стиле ООП. Хранит координаты точки и содержит методы по вычислению
+ * расстояния до заданной точки и вывод на экран координаты текущей точки.
+ */
 public class Point {
-    public double distance(int x1, int y1, int x2, int y2) {
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    }
+
+	private int x;
+	private int y;
+
+	/**
+	 * Конструктор, который принимает начальное состояние объекта "точка".
+	 *
+	 * @param first  координата x
+	 * @param second координата y
+	 */
+	public Point(int first, int second) {
+		this.x = first;
+		this.y = second;
+	}
+
+	/**
+	 * Вычисляем расстояние между текущей и заданной точкой.
+	 *
+	 * @param that - объект точки, до которой измеряем расстояние.
+	 * @return - расстояние между точками.
+	 */
+	public double distance(Point that) {
+		return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+	}
+
+	/**
+	 * Вывести на экран координаты текущей точки.
+	 */
+	public void info() {
+		System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+	}
 }
--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,58 +2,58 @@ package ru.job4j.condition;
 
 public class Triangle {
 
-    /**
-     * Метод вычисления полупериметра по длинам сторон.
-     * <p>
-     * Формула.
-     * <p>
-     * (a + b + c) / 2
-     *
-     * @param a расстояние между точками a b.
-     * @param b расстояние между точками a c.
-     * @param c расстояние между точками b c.
-     * @return полуперимента.
-     */
-    public double period(double a, double b, double c) {
-        return (a + b + c) / 2;
-    }
+	/**
+	 * Метод вычисления полупериметра по длинам сторон.
+	 * <p>
+	 * Формула.
+	 * <p>
+	 * (a + b + c) / 2
+	 *
+	 * @param a расстояние между точками a b.
+	 * @param b расстояние между точками a c.
+	 * @param c расстояние между точками b c.
+	 * @return полуперимента.
+	 */
+	public double period(double a, double b, double c) {
+		return (a + b + c) / 2;
+	}
 
-    /**
-     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
-     * <p>
-     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
-     *
-     * @param a Длина от точки a b.
-     * @param b Длина от точки a c.
-     * @param c Длина от точки b c.
-     * @return boolean существует ли такой треугольник.
-     */
-    private boolean exist(double a, double c, double b) {
+	/**
+	 * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+	 * <p>
+	 * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+	 *
+	 * @param a Длина от точки a b.
+	 * @param b Длина от точки a c.
+	 * @param c Длина от точки b c.
+	 * @return boolean существует ли такой треугольник.
+	 */
+	private boolean exist(double a, double c, double b) {
 //        double max = (a > c ? a : c) > b ? (a > c ? a : c) : b;
 
-        return (a < c + b) && (b < a + c) && (c < a + b) && (a > 0) && (b > 0) && (c > 0);
-    }
+		return (a < c + b) && (b < a + c) && (c < a + b) && (a > 0) && (b > 0) && (c > 0);
+	}
 
-    /**
-     * Метод должен вычислить площадь треугольника.
-     * <p>
-     * Формула.
-     * <p>
-     * √ p *(p - a) * (p - b) * (p - c)
-     * <p>
-     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
-     *
-     * @return Вернуть площадь, если треугольник существует или -1.
-     */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
-        double rsl = -1;
-        double a = new Point().distance(x1, y1, x2, y2);
-        double b = new Point().distance(x2, y2, x3, y3);
-        double c = new Point().distance(x1, y1, x3, y3);
-        double p = period(a, b, c);
-        if (this.exist(a, b, c)) {
-            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        }
-        return rsl;
-    }
+	/**
+	 * Метод должен вычислить площадь треугольника.
+	 * <p>
+	 * Формула.
+	 * <p>
+	 * √ p *(p - a) * (p - b) * (p - c)
+	 * <p>
+	 * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+	 *
+	 * @return Вернуть площадь, если треугольник существует или -1.
+	 */
+	public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+		double rsl = -1;
+		double a = new Point(x1, y1).distance(new Point(x2, y2));
+		double b = new Point(x2, y2).distance(new Point(x3, y3));
+		double c = new Point(x1, y1).distance(new Point(x3, y3));
+		double p = period(a, b, c);
+		if (this.exist(a, b, c)) {
+			rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		}
+		return rsl;
+	}
 }
