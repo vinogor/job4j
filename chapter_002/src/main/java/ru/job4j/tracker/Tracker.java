58c862cc1efabf7766609a636268274619b2dada
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-05-28 16:13:04 +0300

    2. Реализовать класс Tracker[#143278]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,137 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Array-based CRUD storage for items.
+ *
+ * @author Andreev Aleksandr (andreev.aleksandr.spb@gmail.com)
+ * @version 1
+ * @since 29.05.2019
+ */
 public class Tracker {
+
+	private Item[] items = new Item[100];
+	private int position = 0;
+	private static final Random RND = new Random();
+
+	/**
+	 * Adds item to storage and assign to him generated pseudo-unique id.
+	 *
+	 * @param item item to add.
+	 * @return the same item with generated pseudo-unique id.
+	 */
+	public Item add(Item item) {
+		item.setId(generateId());
+		this.items[position] = item;
+		this.position++;
+		return item;
+	}
+
+	/**
+	 * Searches for an element with the given id.
+	 *
+	 * @param id pseudo-unique identifier of item.
+	 * @return item if search success, else return null.
+	 */
+	public Item findItemById(String id) {
+		Item result = null;
+		for (int i = 0; i < position; i++) {
+			if (this.items[i].getId().equals(id)) {
+				result = this.items[i];
+				break;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Return copy of storage without null elements.
+	 *
+	 * @return copy of array "items" without null elements.
+	 */
+	public Item[] findAll() {
+		return Arrays.copyOf(items, position);
+	}
+
+	/**
+	 * Remove item with the given id.
+	 *
+	 * @param id pseudo-unique identifier of item.
+	 * @return true if success, false if not.
+	 */
+	public boolean delete(String id) {
+		boolean result = false;
+		int index = findIndexById(id);
+		if (index != -1) {
+			System.arraycopy(this.items, index + 1, this.items, index, position - index);
+			this.position--;
+			result = true;
+		}
+		return result;
+	}
+
+	/**
+	 * Item in storage with given id is replaced with given item.
+	 * Given item is set given id.
+	 *
+	 * @param id   pseudo-unique identifier of item.
+	 * @param item item to replace.
+	 * @return true if search success, false if not.
+	 */
+	public boolean replace(String id, Item item) {
+		boolean result = false;
+		int index = findIndexById(id);
+		if (index != -1) {
+			item.setId(id);
+			this.items[index] = item;
+			result = true;
+		}
+		return result;
+	}
+
+	/**
+	 * Searches for an elements with the given name.
+	 *
+	 * @param name to search.
+	 * @return array of items with given name.
+	 */
+	public Item[] findItemsByName(String name) {
+		Item[] itemsCopy = new Item[100];
+		int counter = 0;
+		for (int i = 0; i < position; i++) {
+			if (this.items[i].getName().equals(name)) {
+				itemsCopy[counter] = this.items[i];
+				counter++;
+			}
+		}
+		return Arrays.copyOf(itemsCopy, counter);
+	}
+
+	/**
+	 * Generate pseudo-unique value.
+	 *
+	 * @return pseudo-unique value.
+	 */
+	private String generateId() {
+		return String.valueOf(RND.nextInt() + System.currentTimeMillis());
+	}
+
+	/**
+	 * Searches index of item with the given id.
+	 *
+	 * @param id pseudo-unique identifier of item.
+	 * @return index if search success, -1 if not.
+	 */
+	private int findIndexById(String id) {
+		int result = -1;
+		for (int i = 0; i < position; i++) {
+			if (this.items[i].getId().equals(id)) {
+				result = i;
+				break;
+			}
+		}
+		return result;
+	}
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,130 @@
+package ru.job4j.tracker;
+
+import org.hamcrest.core.IsNull;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TrackerTest {
+
+	private static final String FAKEID = "fakeId";
+	private static final String FAKENAME = "fakeName";
+	private static final String TESTNAME1 = "test1";
+	private static final String TESTNAME2 = "test2";
+	private static final String TESTNAME3 = "test3";
+
+	private Tracker tracker;
+	private Item item1;
+	private Item item2;
+	private Item item3;
+
+	@Before
+	public void setUp() {
+		tracker = new Tracker();
+		item1 = new Item(TESTNAME1, "testDescription1", 1L);
+		item2 = new Item(TESTNAME2, "testDescription2", 2L);
+		item3 = new Item(TESTNAME3, "testDescription3", 3L);
+	}
+
+	@Test
+	public void add() {
+		Item result = tracker.add(item1);
+		assertThat(result.getId(), is(item1.getId()));
+	}
+
+	@Test
+	public void findItemById1() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Item result = tracker.findItemById(item2.getId());
+		assertThat(result, is(item2));
+	}
+
+	@Test
+	public void findItemById2() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Item result = tracker.findItemById(FAKEID);
+		assertThat(result, is(IsNull.nullValue()));
+	}
+
+	@Test
+	public void findAll1() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Item[] result = tracker.findAll();
+		assertThat(result, is(new Item[]{item1, item2, item3}));
+	}
+
+	@Test
+	public void findAll2() {
+		Tracker tracker = new Tracker();
+		Item[] result = tracker.findAll();
+		assertThat(result, is(new Item[0]));
+	}
+
+	@Test
+	public void delete1() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Boolean result = tracker.delete(item2.getId());
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void delete2() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Boolean result = tracker.delete(FAKEID);
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void replace1() {
+		tracker.add(item1);
+		tracker.replace(item1.getId(), item2);
+
+		assertThat(tracker.findItemById(item1.getId()).getName(), is(TESTNAME2));
+	}
+
+	@Test
+	public void replace2() {
+		tracker.add(item1);
+
+		boolean result = tracker.replace(item1.getId(), item2);
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void findItemsByName1() {
+		item3.setName(TESTNAME1);
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Item[] result = tracker.findItemsByName(TESTNAME1);
+		assertThat(result, is(new Item[]{item1, item3}));
+	}
+
+	@Test
+	public void findItemsByName2() {
+		tracker.add(item1);
+		tracker.add(item2);
+		tracker.add(item3);
+
+		Item[] result = tracker.findItemsByName(FAKENAME);
+		assertThat(result, is(new Item[0]));
+	}
+}
