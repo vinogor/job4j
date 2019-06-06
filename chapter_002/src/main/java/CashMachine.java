e3a0c1a0a7b6e746fc6404ec3cec946c3da29dd7
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-06-06 17:12:39 +0300

    экзаменационная задачка "Банкомат размена денег" init commit

---
+++ chapter_002/src/main/java/CashMachine.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//  Реализован банкомат размена денег. Автомат принимает бумажную купюру и обменивает на монеты.
+//
+//  Метод должен возвращать список всех возможных вариантов размена купюры.
+
+public class CashMachine {
+    
+    private final int[] values;
+    
+    public CashMachine(final int[] values) {
+        this.values = values;
+    }
+    
+    public List<List<Integer>> exchange(int note) {
+        return null;
+    }
+}
