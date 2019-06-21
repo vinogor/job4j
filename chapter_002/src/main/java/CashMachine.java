654b0b171a1bb5e0f0b8dd826f4117d70b1ba667
Andreev AS <andreev.aleksandr.spb@gmail.com>
2019-06-21 11:01:09 +0300

    - решение задачи на размен монет - тесты к ней

--- chapter_002/src/main/java/CashMachine.java
+++ chapter_002/src/main/java/CashMachine.java
@@ -1,20 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 //  Реализован банкомат размена денег. Автомат принимает бумажную купюру и обменивает на монеты.
-//
 //  Метод должен возвращать список всех возможных вариантов размена купюры.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CashMachine {
-    
+
     private final int[] values;
-    
+
     public CashMachine(final int[] values) {
         this.values = values;
+        Arrays.sort(this.values);
     }
-    
+
     public List<List<Integer>> exchange(int note) {
-        return null;
+        return exchange(note, this.values[this.values.length - 1]);
+    }
+
+    private List<List<Integer>> exchange(int note, int maxCoin) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (note == 0) {
+            result.add(new ArrayList<>());
+        } else {
+            for (int i = this.values.length - 1; i >= 0; i--) {
+                int coin = values[i];
+                // второе условие убирает повторы (те же монеты, расставленные по другому)
+                if (coin > note || coin > maxCoin) {
+                    continue;
+                }
+                for (List<Integer> remain : exchange(note - coin, coin)) {
+                    List<Integer> set = new ArrayList<>();
+                    set.add(0, coin);
+                    set.addAll(0, remain);
+                    result.add(set);
+                }
+            }
+        }
+        return result;
     }
 }
--- chapter_002/src/test/java/CashMachineTest.java
+++ chapter_002/src/test/java/CashMachineTest.java
@@ -12,32 +12,32 @@ public class CashMachineTest {
         List<List<Integer>> result = machine.exchange(1);
         assertThat(result, is(singletonList(singletonList(1))));
     }
-    
+
     @Test
     public void five() {
         CashMachine machine = new CashMachine(new int[] {1, 5, 10});
         List<List<Integer>> result = machine.exchange(5);
         assertThat(result, is(
-            asList(
-                asList(1, 1, 1, 1, 1),
-                asList(5)
-            )
+                asList(
+                        asList(1, 1, 1, 1, 1),
+                        asList(5)
+                )
         ));
     }
-    
+
     @Test
     public void change() {
         CashMachine machine = new CashMachine(new int[] {10, 5, 1});
         List<List<Integer>> result = machine.exchange(10);
         assertThat(
-            result, is(
-                asList(
-                    singletonList(10),
-                    asList(5, 5),
-                    asList(1, 1, 1, 1, 1, 5),
-                    asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+                result, is(
+                        asList(
+                                singletonList(10),
+                                asList(5, 5),
+                                asList(1, 1, 1, 1, 1, 5),
+                                asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+                        )
                 )
-            )
         );
     }
 }
