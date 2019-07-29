25e0ec1c9d427a86a060abc32cacfa3a40326f68
Andreev AS <andreev.aleksandr.spb@gmail.com>
2019-07-29 10:30:53 +0300

    правки

--- chapter_005/src/main/java/map/User.java
+++ chapter_005/src/main/java/map/User.java
@@ -1,6 +1,7 @@
 package map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -13,23 +14,39 @@ public class User {
         this.birthday = birthday;
     }
 
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        User user = (User) o;
+//
+//        if (children != user.children) return false;
+//        if (!name.equals(user.name)) return false;
+//        return birthday.equals(user.birthday);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = name.hashCode();
+//        result = 31 * result + children;
+//        result = 31 * result + birthday.hashCode();
+//        return result;
+//    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         User user = (User) o;
-
-        if (children != user.children) return false;
-        if (!name.equals(user.name)) return false;
-        return birthday.equals(user.birthday);
+        return children == user.children &&
+            name.equals(user.name) &&
+            birthday.equals(user.birthday);
     }
 
     @Override
     public int hashCode() {
-        int result = name.hashCode();
-        result = 31 * result + children;
-        result = 31 * result + birthday.hashCode();
-        return result;
+        return Objects.hash(name, children, birthday);
     }
 }
