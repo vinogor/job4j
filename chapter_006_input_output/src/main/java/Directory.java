d2625404986e282fd86d67d04ea38b27fa80be47
Andreev AS <andreev.aleksandr.spb@gmail.com>
2019-09-27 11:21:45 +0300

    Мини cmd [#32826]

---
+++ chapter_006_input_output/src/main/java/Directory.java
@@ -0,0 +1,27 @@
+
+// перед запуском добавить -ea в VM options
+
+public class Directory {
+    public static void main(String[] args) {
+
+        final Shell shell = new Shell();
+        assert shell.path().equals("/");
+
+        shell.cd("/");
+        assert shell.path().equals("/");
+
+        shell.cd("usr/..");
+        assert shell.path().equals("/");
+
+        shell.cd("usr").cd("local");
+
+        shell.cd("../local").cd("./");
+        assert shell.path().equals("/usr/local");
+
+        shell.cd("..");
+        assert shell.path().equals("/usr");
+
+        shell.cd("//lib///");
+        assert shell.path().equals("/lib");
+    }
+}
