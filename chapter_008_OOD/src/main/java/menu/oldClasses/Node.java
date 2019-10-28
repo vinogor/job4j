b724c2938c54ff4423702b547be9bf1d7c188117
Andreev AS <andreev.aleksandr.spb@gmail.com>
2019-10-28 18:29:16 +0300

    Создать меню.[#156630]

---
+++ chapter_008_OOD/src/main/java/menu/oldClasses/Node.java
@@ -0,0 +1,35 @@
+package menu.oldClasses;
+
+import java.util.*;
+
+public class Node<E extends Comparable<E>> {
+
+    private final List<Node<E>> children = new ArrayList<>();
+    private final E value;
+    private final int level;
+
+    public Node(final E value, final int level) {
+        this.value = value;
+        this.level = level;
+    }
+
+    public void add(Node<E> child) {
+        this.children.add(child);
+    }
+
+    public List<Node<E>> leaves() {
+        return this.children;
+    }
+
+    public boolean eqValue(E that) {
+        return this.value.compareTo(that) == 0;
+    }
+
+    public E getValue() {
+        return this.value;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
