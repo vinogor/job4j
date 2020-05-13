a4c04e84f808c0a9807ad499f280788c7bd28660
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-05-13 09:33:22 +0300

    3. Реализовать механизм программной остановки потока.[#222216]

---
+++ chapter_010_multithreading/src/main/java/ConsoleProgress.java
@@ -0,0 +1,27 @@
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r loading: -");
+                Thread.sleep(500);
+                System.out.print("\r loading: \\");
+                Thread.sleep(500);
+                System.out.print("\r loading: |");
+                Thread.sleep(500);
+                System.out.print("\r loading: /");
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
