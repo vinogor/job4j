b81513204a8133bceae8155fa6d41187b364d6c1
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-05-16 14:21:49 +0300

    1. Проиллюстрировать проблемы с многопоточностью.[#222219]

--- chapter_010_multithreading/src/main/java/jmm/Cache.java
+++ chapter_010_multithreading/src/main/java/jmm/Cache.java
@@ -1,12 +1,35 @@
 package jmm;
 
+// Double Check
 public final class Cache {
-    private static Cache cache;
+
+    private static volatile Cache cache;
+
+    private Cache() {
+    }
 
     private static Cache instOf() {
-        if (cache == null) {
-            cache = new Cache();
+        Cache tempCache = cache;
+        if (tempCache == null) {
+            synchronized (Cache.class) {
+                tempCache = cache;
+                if (tempCache == null) {
+                    cache = tempCache = new Cache();
+                }
+            }
         }
-        return cache;
+        return tempCache;
+    }
+}
+
+// On Demand Holder
+final class Cache2 {
+
+    public static class SingletonHolder {
+        public static final Cache2 INSTANCE = new Cache2();
+    }
+
+    public static Cache2 getInstance() {
+        return SingletonHolder.INSTANCE;
     }
 }
